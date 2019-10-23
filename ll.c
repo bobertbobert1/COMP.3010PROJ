321b0e0ba723f08176e6b61b2a91ace613870fbc
bobertbobert1 <55214560+bobertbobert1@users.noreply.github.com>
2019-10-22 21:12:26 -0400

    Add files via upload
    Completed Task 19 - Defined structures in LL for J1

---
+++ ll.c
@@ -0,0 +1,43 @@
+/*
+Nicholas Sweeney LL Code
+*/
+
+#include <stdio.h>
+
+
+typedef enum(FALSE, TRUE)bool;
+typedef enum{plus, sub, mult, div, less, lessE, equal, greatE, great}prim;
+
+struct
+{
+	int type;
+	int n;
+	bool b;
+	prim p;
+		
+}v;
+	
+struct
+{
+	e* eee;
+	
+}JApp;
+	
+struct
+{
+	bool cond;
+	e taction;
+	e faction;
+}Jif;
+	
+struct
+{
+	int type;
+	JApp a;
+	Jif i;
+}e;
+int main(int argc, char* argv[])
+{
+	printf("Initial commit p0g\n");
+	
+}
